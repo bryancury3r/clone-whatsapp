2985500e7ae876b05039bd230f9ee2d2e52f09ae
bryancury3r <gabriel_bryancury@outlook.com>
2022-02-08 00:30:15 -0300

    adicionando image

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () => {
+  return (
+    <div className="chatIntro">
+      <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Connection" />
+      <h1>Matenha seu celular conectado</h1>
+      <h2>O WhatsApp conecta ao seu telefone para sincronizar suas mensagens.
+        Para reduzir o uso de dados, conecte seu telefone a uma rede WI-FI.
+      </h2>
+    </div>
+  );
+}
